e3e2b0f821a661830c74f64b4717d60cd4a214db
Netra Kc <71728112+Netra-Bahadur-khatri@users.noreply.github.com>
2021-02-19 15:45:39 +0545

    Add files via upload

---
+++ 02_Introduction_JS/script.js
@@ -0,0 +1,32 @@
+
+console.log("Netra is an good boy.");
+
+// undefined in console js means it did not return any things
+
+console.time("My code tooks: ")// #1-->Related
+
+console.log(34+8)
+console.log(45)
+console.log("Printing an Javscript arrays: ")
+console.log([34,1,2,3,4,5,6,7,8])
+
+console.log("Printing an js objects: ")
+console.log({Netra:"12", 2:"Khatri"});
+
+// this will print an objects in the table format.
+console.log("Printing an js objects in table format: ")
+console.table({Name:"Netra", Address:"Banke", Collge_Name:"Tbc"});
+
+// Printing an warning in js console
+console.log("Displaying an warning in console.");
+// console.warn("This is an warning.");
+
+// Clearing an console in js
+// console.clear();
+
+console.timeEnd("My code tooks: "); //#1--> Related
+
+// 
+// console.assert(567<189, "This is not possible in earth.");
+// console.error("This is an error");
+
